77cb549ce494f4f66bf8452d2c3beed5a643e76d
Pedro José Anula <pedrojanula@gmail.com>
2017-10-17 16:55:50 +0200

    Created PM Controller and added a basic template to inbox.

---
+++ src/AppBundle/Controller/PMController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @Route("/private")
+ */
+class PMController extends Controller
+{
+    /**
+     * @Route("/", name="inbox")
+     */
+    public function inboxAction(Request $request)
+    {
+        $user = $this->getUser();
+
+        $PM = $this->getDoctrine()->getRepository('AppBundle:PM')->findBy(
+            array(
+                'recipient' => $user,
+                'reply' => null,
+                'isDeletedByRecipient' => false
+            )
+        );
+
+        return $this->render('PM/inbox.html.twig',
+            array(
+                'user' => $user,
+                'pmList' => $PM
+            )
+        );
+    }
+}
