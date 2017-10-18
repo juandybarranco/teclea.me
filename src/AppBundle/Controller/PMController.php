cfd8efb2826245c60be5ebf338d8d2cd04f6a3d4
Pedro José Anula <pedrojanula@gmail.com>
2017-10-18 17:01:18 +0200

    Added new function: Mark a private message as Read or Not Read.

--- src/AppBundle/Controller/PMController.php
+++ src/AppBundle/Controller/PMController.php
@@ -45,4 +45,58 @@ class PMController extends Controller
             )
         );
     }
+
+    /**
+ * @Route("/markAsRead/{id}", name="markPMAsRead")
+ */
+    public function markAsRead(Request $request, $id)
+    {
+        $user = $this->getUser();
+
+        $PM = $this->getDoctrine()->getRepository('AppBundle:PM')->findBy(
+            array(
+                'recipient' => $user,
+                'isDeletedByRecipient' => false,
+                'isRead' => false,
+                'id' => $id
+            )
+        );
+
+        if(count($PM) == 1){
+            $PM[0]->setIsRead(1);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($PM[0]);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('inbox');
+    }
+
+    /**
+     * @Route("/markAsNotRead/{id}", name="markPMAsNotRead")
+     */
+    public function markAsNotRead(Request $request, $id)
+    {
+        $user = $this->getUser();
+
+        $PM = $this->getDoctrine()->getRepository('AppBundle:PM')->findBy(
+            array(
+                'recipient' => $user,
+                'isDeletedByRecipient' => false,
+                'isRead' => true,
+                'id' => $id
+            )
+        );
+
+        if(count($PM) == 1){
+            $PM[0]->setIsRead(0);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($PM[0]);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('inbox');
+    }
 }
