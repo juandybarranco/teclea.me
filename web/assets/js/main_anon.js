31a0a7f07374ae46ffc14abde09868b38933c718
Pedro José Anula <pedrojanula@gmail.com>
2017-10-31 10:29:03 +0100

    Added a first version of FAQ.

--- web/assets/js/main_anon.js
+++ web/assets/js/main_anon.js
@@ -3,6 +3,13 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+$("document").ready(function(){
+	$("#accordion").accordion({
+		collapsible: true,
+		active: false,
+        heightStyle: "content"
+	})
+});
 
 (function($) {
 
