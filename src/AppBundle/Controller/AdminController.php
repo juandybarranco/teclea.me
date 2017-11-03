feb1457b9239821bc52dd75d9e03ab658e753669
Pedro José Anula <pedrojanula@gmail.com>
2017-11-03 10:59:09 +0100

    Implemented: Suspend and Activate User (Admin).

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -18,11 +18,11 @@ class AdminController extends Controller
     {
         $user = $this->getUser();
 
-        if($user->getIsAdmin()){
+        if ($user->getIsAdmin()) {
             $messages = $this->getDoctrine()->getRepository('AppBundle:Message')->findAll();
 
             $users = $this->getDoctrine()->getRepository('AppBundle:User')->findAll();
-        }else{
+        } else {
             return $this->redirectToRoute('index');
         }
 
@@ -36,13 +36,13 @@ class AdminController extends Controller
     /**
      * @Route("/users", name="usersList")
      */
-    public function  usersListAction()
+    public function usersListAction()
     {
         $user = $this->getUser();
 
-        if(!$user->getIsAdmin()){
+        if (!$user->getIsAdmin()) {
             return $this->redirectToRoute('index');
-        }else{
+        } else {
             $users = $this->getDoctrine()->getRepository('AppBundle:User')->findAll();
         }
 
@@ -50,5 +50,102 @@ class AdminController extends Controller
             'user' => $user,
             'users' => $users
         ]);
+    }
+
+    /**
+     * @Route("/user", name="userNULL")
+     */
+    public function userNULL()
+    {
+        return $this->redirectToRoute('usersList');
+    }
+
+    /**
+     * @Route("/user/{id}", name="userDetailsAdmin")
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function userDetailsAdminAction($id)
+    {
+        $user = $this->getUser();
+
+        if (!$user->getIsAdmin()) {
+            return $this->redirectToRoute('index');
+        } else {
+            $userInfo = $this->getDoctrine()->getRepository('AppBundle:User')->find($id);
+
+            if (!$userInfo) {
+                return $this->redirectToRoute('usersList');
+            }
+        }
+
+        return $this->render('Admin/user.html.twig', [
+            'user' => $user,
+            'userInfo' => $userInfo
+        ]);
+    }
+
+    /**
+     * @Route("/user/{id}/suspend", name="suspendUser")
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function suspendUserAction($id)
+    {
+        $user = $this->getUser();
+
+        if (!$user->getIsAdmin()) {
+            return $this->redirectToRoute('index');
+        } else {
+            $userInfo = $this->getDoctrine()->getRepository('AppBundle:User')->find($id);
+
+            if (!$userInfo) {
+                return $this->redirectToRoute('usersList');
+            }else{
+                if(!$userInfo->isIsSuspended()){
+                    $userInfo->setIsSuspended(1);
+
+                    $em = $this->getDoctrine()->getManager();
+                    $em->persist($userInfo);
+                    $em->flush();
+                }
+
+                return $this->redirectToRoute('userDetailsAdmin', ['id' => $userInfo->getId()]);
+            }
+        }
+    }
+
+    /**
+     * @Route("/user/{id}/activate", name="activateUser")
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function activateUserAction($id)
+    {
+        $user = $this->getUser();
+
+        if (!$user->getIsAdmin()) {
+            return $this->redirectToRoute('index');
+        } else {
+            $userInfo = $this->getDoctrine()->getRepository('AppBundle:User')->find($id);
+
+            if (!$userInfo) {
+                return $this->redirectToRoute('usersList');
+            }else{
+                if($userInfo->isIsSuspended()){
+                    $userInfo->setIsSuspended(0);
+                }
+
+                if($userInfo->isIsBlock()){
+                    $userInfo->setIsBlock(0);
+                }
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($userInfo);
+                $em->flush();
+
+                return $this->redirectToRoute('userDetailsAdmin', ['id' => $userInfo->getId()]);
+            }
+        }
     }
 }
